6a33c6b14e6371807e7978ec613359359223b403
danielSainero <sainero2002dani.j@gmail.com>
2021-05-14 00:14:13 +0200

    Inicio de Proyecto

--- battleShip/Form1.cs
+++ battleShip/Form1.cs
@@ -15,6 +15,43 @@ namespace battleShip
         public Form1()
         {
             InitializeComponent();
+            crearTablero();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            
+                PictureBox pictures = sender as PictureBox;
+                if (pictures != null)
+                {
+                    pictures.Image = Image.FromFile("./../../img/barco.jpg");
+                    pictures.Tag = "B";
+                    MessageBox.Show(pictures.Tag.ToString());
+                    
+                }
+            
+        }
+
+        public void crearTablero ()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                PictureBox pictures = control as PictureBox;
+                if (pictures != null)
+                {
+                    pictures.Tag = 'A';
+                    pictures.Image = Image.FromFile("./../../img/mar.jpg");
+                }
+            }
         }
     }
+  
 }
